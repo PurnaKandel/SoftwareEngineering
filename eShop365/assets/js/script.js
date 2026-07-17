10adef9654546761ff91fcbd577ca96d70d28d18
Purna Kandel <pb.kandel@gmail.com>
2026-07-17 17:29:51 +0545

    product card modified

--- eShop365/assets/js/script.js
+++ eShop365/assets/js/script.js
@@ -1,7 +1,7 @@
 let cartCount=0;
 const cartCounter = document.getElementById("cart-count");
 const addButton = document.getElementById("add-cart");
-addButton.addEventListener("click", function() {
+addButton.addEventListener("click", function(0) {
     cartCount++;
     cartCounter.textContent=cartCount;
 });
