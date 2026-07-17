e26cc2343b72b487c0f4484ff0b43bc8897f62ee
Purna Kandel <pb.kandel@gmail.com>
2026-07-17 17:44:47 +0545

    add product features

--- eShop365/assets/js/script.js
+++ eShop365/assets/js/script.js
@@ -1,7 +1,7 @@
 let cartCount=0;
 const cartCounter = document.getElementById("cart-count");
 const addButton = document.getElementById("add-cart");
-addButton.addEventListener("click", function(0) {
+addButton.addEventListener("click", function() {
     cartCount++;
     cartCounter.textContent=cartCount;
 });
