66d5dd1e1b2b2a363ef1493cec49899bfda635c8
Purna Kandel <pb.kandel@gmail.com>
2026-07-18 06:33:15 +0545

    Add product container to position the product card

--- eShop365/assets/js/script.js
+++ eShop365/assets/js/script.js
@@ -1,7 +1,17 @@
 let cartCount=0;
-const cartCounter = document.getElementById("cart-count");
-const addButton = document.getElementById("add-cart");
-addButton.addEventListener("click", function() {
-    cartCount++;
-    cartCounter.textContent=cartCount;
+//getElementById() for one element and queryselectorall() for many elements
+//const cartCounter = document.getElementById("cart-count");
+//const addButton = document.getElementById("add-cart");
+//addButton.addEventListener("click", function() {
+  //  cartCount++;
+ //   cartCounter.textContent=cartCount;
+//});
+
+const button=document.querySelectorAll(".add-to-cart-button");
+button.forEach(button=>{
+    button.addEventListener("click", function(){
+        //Add product to cart
+        cartCount++;
+        cartCounter.textContent=cartCount;
+    });
 });
